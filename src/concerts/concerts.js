b4a82d9d55e42c8cc1e39900894687c7609b3f71
Brian Neal <brianbneal@gmail.com>
2019-01-25 11:10:23 -0600

    working to filter search of concert API

--- src/concerts/concerts.js
+++ src/concerts/concerts.js
@@ -1,8 +1,15 @@
-fetch ("https://app.ticketmaster.com/discovery/v2/events.json?city=nashville&startDateTime=2019-01-24T06:00:01Z&endDateTime=2019-01-25T06:00:00Z&keyword=music&apikey=MqDKE2kPYJihkI1bA4caKHsLSKNQjBoW")
+function pad(d) {
+    return (d < 10) ? '0' + d.toString() : d.toString();
+}
+
+
+fetch ("https://app.ticketmaster.com/discovery/v2/events.json?city=nashville&startDateTime=2019-01-25T06:00:01Z&endDateTime=2019-01-26T06:00:00Z&keyword=music&apikey=MqDKE2kPYJihkI1bA4caKHsLSKNQjBoW")
     .then(response => response.json())
     .then(parsedData => {
         console.log(parsedData)
     })
+
+
 
     // .com/discovery/v2/events.json?city=nashville&
     // "https://app.ticketmaster.com/discovery/v2/events.json?city=nashville&startDateTime=2019-01-24T06:00:01Z&endDateTime=2019-01-25T06:00:00Z&keyword=music&apikey=MqDKE2kPYJihkI1bA4caKHsLSKNQjBoW"
